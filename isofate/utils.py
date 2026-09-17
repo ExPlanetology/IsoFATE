be443af8ef6e8644414ddf490bfc0d02e76aa421
Dan J Bower <djbower@users.noreply.github.com>
2026-09-17 09:46:22 +0200

    refactor: utils module

---
+++ isofate/utils.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2026 Collin Cherubim <collinc@uchicago.edu>
+# SPDX-FileCopyrightText: 2026 Dan J. Bower <dbower@eaps.ethz.ch>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Small shared calculations used across isofate's plain and JAX implementations."""
+
+from jax.typing import ArrayLike
+
+from isofate.constants import const
+
+
+def gravitational_acceleration(mass: ArrayLike, radius: ArrayLike) -> ArrayLike:
+    """Gravitational acceleration at `radius` from a point/enclosed mass `mass`.
+
+    Args:
+        mass: Enclosed mass [kg]
+        radius: Radial distance from the center [m]
+
+    Returns:
+        Gravitational field strength [m/s2]
+    """
+    return const.G * mass / radius**2
