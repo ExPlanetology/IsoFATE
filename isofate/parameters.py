758118862f0111407ef31dd8dc149cee3c3a78d7
Dan J Bower <djbower@users.noreply.github.com>
2026-09-19 21:50:18 +0200

    fix: keyword args for some classes

--- isofate/parameters.py
+++ isofate/parameters.py
@@ -5,16 +5,21 @@
 
 """Parameters"""
 
+import dataclasses
+
 import equinox as eqx
 
 from isofate.escape import EscapeMechanism
 from isofate.escape_core import EscapeNumberFlux
 from isofate.options import IsocalcOptions
+from isofate.species import IsoFATESpecies
 from isofate.system import System
 
 
 class Parameters(eqx.Module):
     system: System
+    _: dataclasses.KW_ONLY
+    isofate_species: IsoFATESpecies = IsoFATESpecies()
     escape_mechanism: EscapeMechanism
     escape_number_flux: EscapeNumberFlux = EscapeNumberFlux()
     isocalc_options: IsocalcOptions = IsocalcOptions()
